d9b40265cc561e592a73decc5a0375d7cb1380b9
Krishna, Bharath | Krish | CPD <bharath.krishna@rakuten.com>
2020-06-19 12:49:29 +0900

    added docker file

--- application/handlers/hello.py
+++ application/handlers/hello.py
@@ -9,8 +9,6 @@ root_path = '/hello'
 class HelloHandler(web.View):
     async def get(self):
         ret = {"greet": "hello", "message": "welcome..!"}
-        logging.info("accessed hello endpoint")
-        
         return web.json_response(ret)
 
 app.router.add_get('', HelloHandler)
